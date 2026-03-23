8a34a577144bb7c47a56b6f84a04846b04154117
Alicia2202 <alicialf02@gmail.com>
2026-03-23 16:46:29 +0000

    Feat: Añadido input de insertar usuario

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -4,8 +4,9 @@ import React, { useEffect, useState } from "react";
 const Home = () => {
 
 	const [users, setUsers] = useState([])
+	const [newUser, setNewUser] = useState('')
 
-	useEffect(() => {getUsers()}, [])
+	useEffect(() => { getUsers() }, [])
 	// Pedido Get
 	const getUsers = () => {
 		fetch('https://playground.4geeks.com/todo/users')
@@ -19,8 +20,38 @@ const Home = () => {
 
 	console.log(users)
 
+	//Pedido Post para crear usuario
+
+	const handleChange = e => {
+		setNewUser(e.target.value)
+	}
+
+	const handleSubmit = e => {
+		e.preventDefault()
+		fetch('https://playground.4geeks.com/todo/users/' + newUser, {
+			method: "POST",
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: {}
+		})
+			.then(resp => {
+				if (!resp.ok) throw new Error('error en el pedido')
+				return resp.json()
+			})
+			.then(data => {
+				console.log(data)
+				getUsers()
+			})
+			.catch(err => console.log(err))
+	}
 	return (
 		<div className="text-center">
+
+			<form onSubmit={handleSubmit}>
+				<input type="text" value={newUser} onChange={handleChange} />
+				<input type="submit" />
+			</form>
 
 
 
